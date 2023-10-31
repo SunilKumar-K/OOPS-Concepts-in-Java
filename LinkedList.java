08d368bb3193811a3dddc2184f72e5b3dd781673
Sunil Kumar Katta <111487330+SunilKumar-K@users.noreply.github.com>
2023-10-31 20:12:27 +0530

    Added generic linked list in java
    Signed-off-by: Sunil Kumar Katta <111487330+SunilKumar-K@users.noreply.github.com>

---
+++ LinkedList.java
@@ -0,0 +1,118 @@
+public class LinkedList {
+    private Node<T> head;
+    private int size;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    // Add an element to the end of the linked list
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    // Get the size of the linked list
+    public int size() {
+        return size;
+    }
+
+    // Check if the linked list is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Remove an element from the linked list
+    public boolean remove(T data) {
+        if (head == null) {
+            return false;
+        }
+
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(data)) {
+                current.next = current.next.next;
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    // Check if an element is present in the linked list
+    public boolean contains(T data) {
+        Node<T> current = head;
+        while (current != null) {
+            if (current.data.equals(data)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    // Get an element at a specific index
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    // Print the linked list
+    public void print() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println("Size: " + list.size());
+        list.print();
+
+        list.remove(2);
+        System.out.println("After removing 2:");
+        System.out.println("Size: " + list.size());
+        list.print();
+    }
+}
